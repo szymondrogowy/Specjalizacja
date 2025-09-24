d52fce09d72656dcbded4e3ceaef2bcf0427a785
szymondrogowy <105376125+szymondrogowy@users.noreply.github.com>
2025-09-24 12:12:28 +0200

    Add files via upload

---
+++ krypto.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twoje akcje</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Kalkulator Kryptowalut</h1>
+        <h3>Przelicz swoje kryptowaluty na stablecoiny USDC/USDT lub PLN</h3>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Strona główna</a></li>
+            <li><a href="kurs.php">Kursy walut</a></li>
+            <li><a href="akcje.php">Akcje spółek</a></li>
+            <li><a href="krypto.php">Krypto</a></li>
+        </ul>
+    </nav>
+
+    W krótce dostępne!
+
+    <footer>
+        <p>Stronę wykonał: Szymon Dróżga</p>
+    </footer>
+</body>
+</html>
