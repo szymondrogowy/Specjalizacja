4ad4da2f2668cfc44da759df87279e84843d5689
szymondrogowy <105376125+szymondrogowy@users.noreply.github.com>
2026-03-11 13:15:24 +0100

    Add files via upload

---
+++ wzrost_procentowy04/templates_c/a6d3956de349f9bd82f192abebafe6eef041c050_0.file_calc.html.php
@@ -0,0 +1,132 @@
+<?php
+/* Smarty version 5.4.2, created on 2026-03-11 12:19:20
+  from 'file:C:\xampp\htdocs\php_04_uproszczony/app/calc.html' */
+
+/* @var \Smarty\Template $_smarty_tpl */
+if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
+  'version' => '5.4.2',
+  'unifunc' => 'content_69b14fb8e7d103_71373337',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a6d3956de349f9bd82f192abebafe6eef041c050' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_04_uproszczony/app/calc.html',
+      1 => 1773227867,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+))) {
+function content_69b14fb8e7d103_71373337 (\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony\\app';
+$_smarty_tpl->getInheritance()->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_74355714869b14fb8e4f065_48046469', 'footer');
+?>
+
+
+<?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_129543423069b14fb8e57754_28365790', 'content');
+$_smarty_tpl->getInheritance()->endChild($_smarty_tpl, "../templates/main.html", $_smarty_current_dir);
+}
+/* {block 'footer'} */
+class Block_74355714869b14fb8e4f065_48046469 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony\\app';
+?>
+Stopka: Kalkulator wzrostu procentowego<?php
+}
+}
+/* {/block 'footer'} */
+/* {block 'content'} */
+class Block_129543423069b14fb8e57754_28365790 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony\\app';
+?>
+
+
+<h2 class="content-head multi-column-center">Kalkulator wzrostu procentowego</h2>
+
+<div class="pure-g">
+<div class="l-box-lrg pure-u-1 pure-u-med-2-5">
+	<form class="pure-form pure-form-stacked" action="<?php echo $_smarty_tpl->getValue('app_url');?>
+/app/calc.php" method="post">
+		<fieldset>
+			<label for="v1">Wartość początkowa</label>
+			<input id="v1" type="text" placeholder="np. 100" name="v1" value="<?php echo $_smarty_tpl->getValue('form')['v1'];?>
+">
+			
+			<label for="v2">Wartość końcowa</label>
+			<input id="v2" type="text" placeholder="np. 120" name="v2" value="<?php echo $_smarty_tpl->getValue('form')['v2'];?>
+">
+		</fieldset>
+		<button type="submit" class="pure-button pure-button-primary">Oblicz wzrost</button>
+	</form>
+</div>
+
+<div class="l-box-lrg pure-u-1 pure-u-med-3-5">
+<div class="messages">
+
+<?php if ((null !== ($_smarty_tpl->getValue('messages') ?? null))) {?>
+	<?php if ($_smarty_tpl->getSmarty()->getModifierCallback('count')($_smarty_tpl->getValue('messages')) > 0) {?> 
+		<h4>Wystąpiły błędy: </h4>
+		<ol class="err">
+		<?php
+$_from = $_smarty_tpl->getSmarty()->getRuntime('Foreach')->init($_smarty_tpl, $_smarty_tpl->getValue('messages'), 'msg');
+$foreach0DoElse = true;
+foreach ($_from ?? [] as $_smarty_tpl->getVariable('msg')->value) {
+$foreach0DoElse = false;
+?>
+		<li><?php echo $_smarty_tpl->getValue('msg');?>
+</li>
+		<?php
+}
+$_smarty_tpl->getSmarty()->getRuntime('Foreach')->restore($_smarty_tpl, 1);?>
+		</ol>
+	<?php }
+}?>
+
+<?php if ((null !== ($_smarty_tpl->getValue('infos') ?? null))) {?>
+	<?php if ($_smarty_tpl->getSmarty()->getModifierCallback('count')($_smarty_tpl->getValue('infos')) > 0) {?> 
+		<h4>Informacje: </h4>
+		<ol class="inf">
+		<?php
+$_from = $_smarty_tpl->getSmarty()->getRuntime('Foreach')->init($_smarty_tpl, $_smarty_tpl->getValue('infos'), 'msg');
+$foreach1DoElse = true;
+foreach ($_from ?? [] as $_smarty_tpl->getVariable('msg')->value) {
+$foreach1DoElse = false;
+?>
+		<li><?php echo $_smarty_tpl->getValue('msg');?>
+</li>
+		<?php
+}
+$_smarty_tpl->getSmarty()->getRuntime('Foreach')->restore($_smarty_tpl, 1);?>
+		</ol>
+	<?php }
+}?>
+
+<?php if ((null !== ($_smarty_tpl->getValue('result') ?? null))) {?>
+	<h4>Wynik wzrostu:</h4>
+	<p class="res">
+	Zmiana wynosi: <strong><?php echo $_smarty_tpl->getValue('result');?>
+%</strong>
+	</p>
+<?php }?>
+
+</div>
+</div>
+</div>
+
+<?php
+}
+}
+/* {/block 'content'} */
+}
---
+++ wzrost_procentowy04/templates_c/f91f463f1a3e689bb58bfb33801fafcf08a66e4b_0.file_main.html.php
@@ -0,0 +1,94 @@
+<?php
+/* Smarty version 5.4.2, created on 2026-03-11 12:18:22
+  from 'file:C:\xampp\htdocs\php_04_uproszczony_new\php_04_uproszczony\app\../templates/main.html' */
+
+/* @var \Smarty\Template $_smarty_tpl */
+if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
+  'version' => '5.4.2',
+  'unifunc' => 'content_69b14f7edda768_78192576',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f91f463f1a3e689bb58bfb33801fafcf08a66e4b' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_04_uproszczony_new\\php_04_uproszczony\\app\\../templates/main.html',
+      1 => 1742287280,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+))) {
+function content_69b14f7edda768_78192576 (\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony_new\\php_04_uproszczony\\templates';
+$_smarty_tpl->getInheritance()->init($_smarty_tpl, false);
+?>
+<!doctype html>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (($tmp = $_smarty_tpl->getValue('page_description') ?? null)===null||$tmp==='' ? 'Opis domyślny' ?? null : $tmp);?>
+">
+	<title><?php echo (($tmp = $_smarty_tpl->getValue('page_title') ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</title>
+	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo $_smarty_tpl->getValue('app_url');?>
+/css/style.css">	
+</head>
+<body>
+
+<div class="header">
+	<h1><?php echo (($tmp = $_smarty_tpl->getValue('page_title') ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</h1>
+	<h2><?php echo (($tmp = $_smarty_tpl->getValue('page_header') ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</h1>
+	<p>
+		<?php echo (($tmp = $_smarty_tpl->getValue('page_description') ?? null)===null||$tmp==='' ? "Opis domyślny" ?? null : $tmp);?>
+
+	</p>
+</div>
+
+<div class="content">
+<?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_30838519269b14f7edd82b0_03077810', 'content');
+?>
+
+</div><!-- content -->
+
+<div class="footer">
+	<p>
+<?php 
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_8908695069b14f7edd9909_17832076', 'footer');
+?>
+
+	</p>
+	<p>
+		Widok oparty na stylach <a href="http://purecss.io/" target="_blank">Pure CSS Yahoo!</a>. (autor przykładu: Przemysław Kudłacik)
+	</p>
+</div>
+
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_30838519269b14f7edd82b0_03077810 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony_new\\php_04_uproszczony\\templates';
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+/* {block 'footer'} */
+class Block_8908695069b14f7edd9909_17832076 extends \Smarty\Runtime\Block
+{
+public function callBlock(\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\php_04_uproszczony_new\\php_04_uproszczony\\templates';
+?>
+ Domyślna treść stopki .... <?php
+}
+}
+/* {/block 'footer'} */
+}
